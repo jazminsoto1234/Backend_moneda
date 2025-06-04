27de1ce04bb3887222a2b24a30f34da066d021e0
jazminsoto <jazmin.soto@utec.edu.pe>
2025-06-04 01:32:00 -0500

    "AAAA"

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -46,10 +46,19 @@ def register():
     }), 201
 
 
-
-
-
 @user_bp.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data["email"]).first()
+    
+    if user and user.check_password(data["password"]):
+        token = create_access_token(identity=str(user.id))
+        return jsonify({"access_token": token})
+    
+    return jsonify({"error": "Credenciales inválidas"}), 401
+
+
+"""@user_bp.route("/login", methods=["POST"])
 def login():
     data = request.json
     user = User.query.filter_by(username=data["username"]).first()
@@ -57,7 +66,7 @@ def login():
         token = create_access_token(identity=str(user.id))
         return jsonify({"access_token": token})
     return jsonify({"error": "Credenciales inválidas"}), 401
-
+"""
 
 
 @user_bp.route("/account", methods=["GET"])
